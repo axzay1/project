a6b6f6e67cfc97b3bcde68cd04039d8ab4ceb217
Akshay Vachhani <akshay.vachhani@coforge.com>
2023-08-09 12:28:26 +0530

    check

--- app.py
+++ app.py
@@ -6,18 +6,14 @@ CORS(app)
 # Import the API routes
 from api import user, products, favorites
 
-# , subscription, brand, store, kitchen, sales
+
 
 # Register the API blueprints
 app.register_blueprint(user.bp)
 app.register_blueprint(products.bp)
 app.register_blueprint(favorites.bp) 
-# app.register_blueprint(subscription.bp)
-# app.register_blueprint(brand.bp)
-# app.register_blueprint(store.bp)
-# app.register_blueprint(kitchen.bp)
-# app.register_blueprint(sales.bp)
+
 
 if __name__ == '__main__':
-     app.run(port=6000, debug=True)
+    app.run(host='0.0.0.0', port=6000)
 
